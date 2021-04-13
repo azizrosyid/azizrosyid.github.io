1310a7515c328ab0657f0cae834ef59b76cf30f5
Aziz Rosyid <azizrosyid783@gmail.com>
2021-04-13 12:43:22 +0700

    finish layout mobile

--- portfoliowebsite/script.js
+++ portfoliowebsite/script.js
@@ -4,4 +4,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
     this.classList.toggle("bg-close");
     document.querySelector(".menu-navbar").classList.toggle("display-on");
   });
+  window.onscroll = function () {
+    const nav = document.querySelector(".wrap-navbar");
+    if (window.pageYOffset > 100) {
+      nav.classList.add("bg-grey");
+    } else {
+      nav.classList.remove("bg-grey");
+    }
+  };
 });
